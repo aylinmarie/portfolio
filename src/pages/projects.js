53e0993b2064c9db3f11df718eeb7e4ac8d11649
Aylin Atkins <aylin.atkins@mailchimp.com>
2022-01-27 11:22:46 -0500

    Update data directory and link

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,46 +5,9 @@ import { Link } from "gatsby";
 
 import { ExternalLink } from "@components";
 
-import baImg from "@images/black_artisans.png";
-import portfolioImg from "@images/sample_portfolio.png";
-import knitImg from "@images/elegant_knit.png";
-import dataVizImg from "@images/mailchimpdataviz.png";
+import PROJECT_ITEMS from "../data/work.js";
 
 const Projects = () => {
-  const PROJECT_ITEMS = [
-    {
-      name: "Black Artisans",
-      link: "https://blackartisans.netlify.app/",
-      img: baImg,
-      description:
-        "Project titled Black Artisans featuring black craftspeople around the world.",
-      external: true,
-    },
-    {
-      name: "Elegant Knit",
-      link: "https://www.elegantknit.co",
-      img: knitImg,
-      description:
-        "Project titled Elegant knits sharing my favorite modern knitting patterns.",
-      external: true,
-    },
-    {
-      name: "Sample Portfolio",
-      link: "https://aylin-project-portfolio.netlify.app/",
-      img: portfolioImg,
-      description:
-        "Early iteration of my portfolio website built in Gatsby and designed in Figma.",
-      external: true,
-    },
-    {
-      name: "Mailchimp Data Visualization",
-      link: "/mailchimp/data-viz",
-      img: dataVizImg,
-      description: "Refresh of Mailchimp's data visualization.",
-      external: false,
-    },
-  ];
-
   return (
     <section className="grid">
       <div>

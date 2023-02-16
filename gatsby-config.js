bc84c65ad3755c00e94e009907ce71f6dfd2e50e
Aylin Marie <hello@aylinmarie.co>
2023-02-15 20:24:28 -0500

    Remove prismic

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,3 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-});
-
-const routes = [
-  {
-    type: "ig_links",
-    path: "/ig-links",
-  },
-];
-
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.aylinmarie.co",
@@ -16,18 +5,6 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-image",
-    {
-      resolve: "gatsby-source-prismic",
-      options: {
-        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
-        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
-        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
-        schemas: {
-          my_deleted_schema: {},
-        },
-        routes,
-      },
-    },
     "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
